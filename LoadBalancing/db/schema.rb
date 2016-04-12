7c5f603f96c6d0623f70b14e54401b4a34e42e3f
Brian Yip <itsbriany@gmail.com>
2016-04-12 16:57:07 -0300

    Added scaffolding for the cloud computing model (without authentication)

--- LoadBalancing/db/schema.rb
+++ LoadBalancing/db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160412194303) do
+
+  create_table "cloud_computings", force: :cascade do |t|
+    t.string   "timestamp"
+    t.string   "payload"
+    t.integer  "execution_delay"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
 end
